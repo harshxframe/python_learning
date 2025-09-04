cc99a9cef7e4fadbeda6d112e3157519052ebf5e
scotedflotsin <scotedflotsin.co.ltd@gmail.com>
2025-09-04 23:38:12 +0530

    Projects Exercise improvements

--- Projects/snake_water_gun/main.py
+++ Projects/snake_water_gun/main.py
@@ -12,34 +12,28 @@ def numberPic():
     return randNum
 
 
-computer = numberPic()
-youStr = input("Enter your choice[s, w, g]")
-youDic = {"s":1, "w":-1, "g":0}
+while True:
+    computer = numberPic()
+    youStr = input("Enter your choice[s, w, g]")
+    youDic = {"s":1, "w":-1, "g":0}
+    you = youDic[youStr]
 
 
-you = youDic[youStr]
+    if computer == -1 and you ==1:
+       print("You win")
+    elif computer ==-1 and you == 0:
+       print("You lose")
+    elif computer ==1 and you == -1:
+       print("you lose")
+    elif computer ==1 and you == 0:
+       print("You win")
+    elif computer ==0 and you == -1:
+       print("You lose")
+    elif computer ==0 and you == 1:
+       print("You lose")
+    elif computer == you:
+       print("It's a draw")
+    else:
+       print("Something went wrong")
 
 
-if computer == -1 and you ==1:
-    print("You win")
-elif computer ==-1 and you == 0:
-    print("You lose")
-elif computer ==1 and you == -1:
-    print("you lose")
-elif computer ==1 and you == 0:
-    print("You win")
-elif computer ==0 and you == -1:
-    print("You lose")
-elif computer ==0 and you == 1:
-    print("You lose")
-elif computer == you:
-    print("It's a draw")
-else:
-    print("Something went wrong")
-
-
-
-def numberPic():
-    # to pic appropriate random number
-    randNum = random.randint(-1, 1)
-    return randNum
